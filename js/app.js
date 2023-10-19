33e3cc1e1201a8128d1eb169bd836e0c3efa126a
roomrys <lmaree@ucsd.edu>
2023-10-18 21:34:40 -0700

    Add note names when hovering

--- js/app.js
+++ js/app.js
@@ -4,6 +4,20 @@ const stringStartingNotes = { stringHighE: 'E', stringB: 'B', stringG: 'G', stri
 
 // TODO: Add note aliases (e.g. CSharp = Db)
 const noteNames = ['A', 'ASharp', 'B', 'C', 'CSharp', 'D', 'DSharp', 'E', 'F', 'FSharp', 'G', 'GSharp'];
+const noteNamesWithSharpAsHash = {
+    'A': 'A',
+    'ASharp': 'A#',
+    'B': 'B',
+    'C': 'C',
+    'CSharp': 'C#',
+    'D': 'D',
+    'DSharp': 'D#',
+    'E': 'E',
+    'F': 'F',
+    'FSharp': 'F#',
+    'G': 'G',
+    'GSharp': 'G#'
+};
 const noteColors = { 'A': `rgb(255, 0, 0)`, 'ASharp': `rgb(255, 127, 0)`, 'B': `rgb(255, 255, 0)`, 'C': `rgb(127, 255, 0)`, 'CSharp': `rgb(0, 255, 0)`, 'D': `rgb(0, 255, 127)`, 'DSharp': `rgb(0, 255, 255)`, 'E': `rgb(0, 127, 255)`, 'F': `rgb(0, 0, 255)`, 'FSharp': `rgb(127, 0, 255)`, 'G': `rgb(255, 0, 255)`, 'GSharp': `rgb(255, 0, 127)` };
 
 const guitarNeckElement = document.querySelector('.guitarNeck');
@@ -38,7 +52,9 @@ for (let i = 0; i < numFretsToShow; i++) {
         openNote = stringStartingNotes[stringName];
         fretNoteIndex = noteNames.indexOf(openNote) + i + 1;
         fretNote = noteNames[fretNoteIndex % noteNames.length];
+        fretNoteText = noteNamesWithSharpAsHash[fretNote];
         noteElement.classList.add(`note${fretNote}`);
+        noteElement.setAttribute('data-note', fretNoteText);
 
         // Set background color
         noteElement.style.backgroundColor = noteColors[fretNote];
